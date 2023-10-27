3f65b6e2851b40ee3a5346b1343d601827e73ba0
Will Robertson <williamdrobertson42@gmail.com>
2023-10-27 20:56:28 +1100

    feat: move to day 4

--- src/day4.rs
+++ src/day4.rs
@@ -1,1 +1,19 @@
+// multithread this?
+use md5;
+
 fn main() {
+    let input_str = "iwrupvqb".to_string();
+    let mut counter: i128 = 0;
+
+    loop {
+        counter = counter + 1;
+        let input = input_str.clone() + &counter.to_string();
+        let hash = format!("{:x}", md5::compute(input));
+        let first_five = &hash[0..5];
+
+        if first_five == "000000" {
+            println!("The count is {}", counter);
+            break;
+        }
+    }
+}

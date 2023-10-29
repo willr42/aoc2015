c9e233e1c4bc9935e982664568f3db02bb4981b1
Will Robertson <williamdrobertson42@gmail.com>
2023-10-29 14:43:15 +1100

    add day 6 explanation

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
+/// One million lights to a 1000x1000 grid.
+/// Grid is numbered 0,999 on each dimension
+
+/// Instruction format
+/// turn on 0,0 through 999,999
+/// toggle 0,0 through 999,0 - toggle first line of lights, turning off ones on, turning on ones off
+/// turn off 499,499 through 500,500 - turn off & leave off the middle 4 lights.
 use aoc2015::read_lines;
 
 fn main() {
